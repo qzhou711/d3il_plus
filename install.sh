044b0157484a83e6d167a275d9e7382d2a26a3b6
qzhou711 <bamboosdu@gmail.com>
2026-05-05 21:07:00 -0400

    requirements install

--- install.sh
+++ install.sh
@@ -23,42 +23,42 @@ conda config --add channels conda-forge
 conda config --set channel_priority strict
 
 
-############ PYTHON ############
-echo Install mamba
-conda install mamba -c conda-forge -y -q
+# ############ PYTHON ############
+# echo Install mamba
+# conda install mamba -c conda-forge -y -q
 
 
-############ REQUIRED DEPENDENCIES (PYBULLET) ############
-echo Installing dependencies...
+# ############ REQUIRED DEPENDENCIES (PYBULLET) ############
+# echo Installing dependencies...
 
-mamba install -c conda-forge pytorch==1.13.0 torchvision==0.14.0
+# mamba install -c conda-forge pytorch==1.13.0 torchvision==0.14.0
 
-mamba install -c conda-forge pybullet pyyaml scipy opencv pinocchio matplotlib gin-config gym==0.21.0 -y -q
+# mamba install -c conda-forge pybullet pyyaml scipy opencv pinocchio matplotlib gin-config gym==0.21.0 -y -q
 
-# Open3D for PointClouds and its dependencies. Why does it not install them directly?
-mamba install -c conda-forge scikit-learn addict pandas plyfile tqdm -y -q
-mamba install -c open3d-admin open3d -y -q
+# # Open3D for PointClouds and its dependencies. Why does it not install them directly?
+# mamba install -c conda-forge scikit-learn addict pandas plyfile tqdm -y -q
+# mamba install -c open3d-admin open3d -y -q
 
-pip install einops
-pip install hydra-core==1.1.1
-pip install wandb
+# pip install einops
+# pip install hydra-core==1.1.1
+# pip install wandb
 
-# Robomimic
-pip install termcolor
+# # Robomimic
+# pip install termcolor
 
-# ACT
-pip install ipython
+# # ACT
+# pip install ipython
 
-# BESO
-pip install torchsde torchdiffeq
+# # BESO
+# pip install torchsde torchdiffeq
 
-############ MUJOCO BETA SUPPORT INSTALLATION ############
-mamba install -c conda-forge imageio -y -q
-pip install mujoco==2.3.2
+# ############ MUJOCO BETA SUPPORT INSTALLATION ############
+# mamba install -c conda-forge imageio -y -q
+# pip install mujoco==2.3.2
 
-############ INSTALL D3il-Sim & FINALIZE ############
-echo
-echo Installing D3il-Sim Package
-cd environments/d3il && pip install -e .
+# ############ INSTALL D3il-Sim & FINALIZE ############
+# echo
+# echo Installing D3il-Sim Package
+# cd environments/d3il && pip install -e .
 
 exit 0
